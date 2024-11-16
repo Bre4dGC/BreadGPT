5bdc087785e47de9017e15dd12e6cc3a41c92f63
Bre4dGC <glebbreadc@gmail.com>
2024-11-16 14:08:29 +0300

    Tab, MessageInput, ChatNavigation components created

---
+++ Styles/Compoments/ChatNavButton.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace BreadGPT.Styles.Compoments
+{
+    public partial class ChatNavButton : UserControl
+    {
+        public ChatNavButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Styles/Compoments/MessageInput.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BreadGPT.Styles.Compoments
+{
+    /// <summary>
+    /// Логика взаимодействия для MessageInput.xaml
+    /// </summary>
+    public partial class MessageInput : UserControl
+    {
+        public MessageInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
