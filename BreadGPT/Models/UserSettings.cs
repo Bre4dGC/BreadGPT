62bf04f2b6d7ba33912a4cab82074370b7c96bc7
Bre4dGC <glebbreadc@gmail.com>
2024-11-20 23:17:32 +0300

    Delete UserSettings

--- BreadGPT/Models/UserSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BreadGPT.Models
-{
-    public class UserSettings
-    {
-        public Guid Id { get; set; }
-        public string ApiKey { get; set; } = string.Empty;
-        public bool IsSidebarHidden { get; set; }
-    }
-}
