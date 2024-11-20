57a6d353ece4a60cc73aa3a52b8fcc7e90f79d45
Bre4dGC <glebbreadc@gmail.com>
2024-11-20 23:18:01 +0300

    Update Components

--- BreadGPT/Styles/Compoments/ChatActionsPopup.xaml.cs
+++ BreadGPT/Styles/Compoments/ChatActionsPopup.xaml.cs
@@ -1,26 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace BreadGPT.Styles.Compoments
 {
     /// <summary>
     /// Логика взаимодействия для ChatButtonActions.xaml
     /// </summary>
-    public partial class ChatButtonActions : UserControl
+    public partial class ChatActionPopup : UserControl
     {
-        public ChatButtonActions()
+        public ChatActionPopup()
         {
             InitializeComponent();
         }
