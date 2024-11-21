7d732c39ca3363733218c28a4662d478a24a7152
Bre4dGC <glebbreadc@gmail.com>
2024-11-21 23:05:25 +0300

    Updated Message model

--- BreadGPT/Models/Message.cs
+++ BreadGPT/Models/Message.cs
@@ -5,8 +5,8 @@
         public Guid Id { get; set; }
         public Guid ChatId { get; set; }
         public string Text { get; set; } = string.Empty;
-        public MessageSender Sender { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public MessageSender Sender { get; set; }
         public MessageStatus Status { get; set; }
 
         public virtual Chat Chat { get; set; }
