152115d30d05a0932c99765b75b97d420a35becb
Bre4dGC <glebbreadc@gmail.com>
2024-11-21 23:02:48 +0300

    Created DataContext

---
+++ BreadGPT/Data/BreadGPTDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BreadGPT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BreadGPT.Data
+{
+    class BreadGPTDbContext : DbContext
+    {
+        public DbSet<Chat> Chats { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Chat>()
+                .HasMany(c => c.Messages)
+                .WithOne(m => m.Chat)
+                .HasForeignKey(m => m.ChatId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=BreadgptDB;Integrated Security=True;Trust Server Certificate=True");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
