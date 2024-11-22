6bcb38ea12a324481e2c3ceb41c240867a988f9b
Bre4dGC <glebbreadc@gmail.com>
2024-11-22 23:33:41 +0300

    Created DbContextFactory

---
+++ BreadGPT/Data/BreadgptDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Windows;
+
+namespace BreadGPT.Data
+{
+    internal class BreadgptDbContextFactory : IDesignTimeDbContextFactory<BreadGPTDbContext>
+    {
+        public BreadGPTDbContext CreateDbContext(string[] args = null)
+        {
+            var options = new DbContextOptionsBuilder<BreadGPTDbContext>();
+            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BreadGPTDB;Trusted_Connection=true;");
+
+            return new BreadGPTDbContext(options.Options);
+        }
+    }
+}
