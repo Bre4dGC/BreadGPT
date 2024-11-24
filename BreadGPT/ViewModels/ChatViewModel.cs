5e6f60cbc16c66b6864c7abe02f1cf0588c9312c
Bre4dGC <glebbreadc@gmail.com>
2024-11-25 01:52:36 +0300

    Update ViewModels

---
+++ BreadGPT/ViewModels/ChatViewModel.cs
@@ -0,0 +1,39 @@
+﻿using BreadGPT.Models;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace BreadGPT.ViewModels
+{
+    internal class ChatViewModel : BaseViewModel
+    {
+        public Chat CurrentChat { get; set; }
+        public ObservableCollection<Message> Messages { get; } = new();
+
+        private string _newMessage;
+        public string NewMessage
+        {
+            get { return _newMessage; }
+            set { _newMessage = value; OnPropertyChanged(); }
+        }
+
+        public ICommand SendMessageCommand { get; }
+
+        public ChatViewModel()
+        {
+            SendMessageCommand = new RelayCommand(SendMessage);
+        }
+
+        private void SendMessage()
+        {
+            if (string.IsNullOrWhiteSpace(NewMessage)) return;
+
+            Messages.Add(
+                new Message
+                {
+                    Text = NewMessage,
+                    Sender = MessageSender.User
+                });
+        }
+    }
+}
--- BreadGPT/ViewModels/MainViewModel.cs
+++ BreadGPT/ViewModels/MainViewModel.cs
@@ -3,50 +3,77 @@ using BreadGPT.Models;
 using BreadGPT.Services;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 
 namespace BreadGPT.ViewModels
 {
     class MainViewModel : BaseViewModel
     {
-        private IChatService<Chat> chatService = new ChatService<Chat>(new BreadgptDbContextFactory());
+        private ChatViewModel _selectedChat;
 
-        private Chat _selectedChat;
-
-        public Chat SelectedChat
+        public ChatViewModel SelectedChat
         {
             get { return _selectedChat; }
             set { _selectedChat = value; OnPropertyChanged(); }
         }
 
+        private IChatService<Chat> chatService = new ChatService<Chat>(new BreadgptDbContextFactory());
         public ObservableCollection<Chat> Chats { get; } = new();
 
         public ICommand CreateChatCommand { get; }
         public ICommand RenameChatCommand { get; }
         public ICommand DeleteChatCommand { get; }
+        public ICommand SelectChatCommand { get; }
 
         public MainViewModel()
         {
-            CreateChatCommand = new RelayCommand(CreateChat);
+            LoadChats();
 
+            CreateChatCommand = new RelayCommand(CreateChat);
             RenameChatCommand = new RelayCommand(DeleteChat);
             DeleteChatCommand = new RelayCommand(DeleteChat);
         }
 
-        private void LoadChats()
-        {
-            var chats = chatService.GetAllAsync();
-        }
-
         private void CreateChat()
         {
+            if (Chats.Count == 10) { MessageBox.Show("You have increased the maximum number of chats"); return; }
+
+            Chat chat;
             chatService.CreateAsync(
-                new Chat 
+                chat = new Chat
                 {
                     Id = Guid.NewGuid(),
-                    Title = $"New Chat",
+                    Title = $"New Chat {Chats.Count + 1}",
                     LastMessageAt = DateTime.UtcNow
                 });
+
+            Chats.Add(chat);
+
+            SortChats();
+        }
+
+        private async void LoadChats()
+        {
+            var chats = await chatService.GetAllAsync();
+
+            foreach(var chat in chats)
+            {
+                Chats.Add(chat);
+            }
+
+            SortChats();
+        }
+
+        private void SortChats()
+        {
+            var sorted = Chats.OrderByDescending(chat => chat.LastMessageAt).ToList();
+            Chats.Clear();
+
+            foreach (var chat in sorted)
+            {
+                Chats.Add(chat);
+            }
         }
 
         private void DeleteChat()
