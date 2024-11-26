ba876ee725a61a27f8a27424b9383c1d47781dfa
Bre4dGC <glebbreadc@gmail.com>
2024-11-26 23:29:56 +0300

    Update VIewModels

--- BreadGPT/ViewModels/ChatViewModel.cs
+++ BreadGPT/ViewModels/ChatViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using BreadGPT.Models;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Windows.Input;
 
 namespace BreadGPT.ViewModels
 {
-    internal class ChatViewModel : BaseViewModel
+    public class ChatViewModel : BaseViewModel
     {
         public Chat CurrentChat { get; set; }
         public ObservableCollection<Message> Messages { get; } = new();
@@ -19,12 +22,13 @@ namespace BreadGPT.ViewModels
 
         public ICommand SendMessageCommand { get; }
 
-        public ChatViewModel()
+        public ChatViewModel(Chat chat)
         {
+            CurrentChat = chat;
             SendMessageCommand = new RelayCommand(SendMessage);
         }
 
-        private void SendMessage()
+        private async void SendMessage()
         {
             if (string.IsNullOrWhiteSpace(NewMessage)) return;
 
@@ -33,6 +37,35 @@ namespace BreadGPT.ViewModels
                 {
                     Text = NewMessage,
                     Sender = MessageSender.User
+                });
+
+            NewMessage = string.Empty;
+
+            await GetResponse(NewMessage);
+        }
+
+        private async Task GetResponse(string message)
+        {
+            var json = JsonSerializer.Serialize(new
+            {
+                model = "mistral-large-latest",
+                messages = new { role = "user", content = message }
+            });
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.mistral.ai/v1/chat/completions");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer r15uvSJsdSTGKygysJWuJqKRZjQGAKEm");
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var responseMessage = await client.SendAsync(new HttpRequestMessage(HttpMethod.Post, (string)null!) { Content = content });
+
+
+            Messages.Add(
+                new Message
+                {
+                    Text = await responseMessage.Content.ReadAsStringAsync(),
+                    Sender = MessageSender.ChatGPT
                 });
         }
     }
--- BreadGPT/ViewModels/MainViewModel.cs
+++ BreadGPT/ViewModels/MainViewModel.cs
@@ -1,63 +1,81 @@
-﻿using BreadGPT.Data;
+﻿using BreadGPT.Commands;
+using BreadGPT.Data;
 using BreadGPT.Models;
 using BreadGPT.Services;
+using BreadGPT.Views;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Navigation;
 
 namespace BreadGPT.ViewModels
 {
     class MainViewModel : BaseViewModel
     {
-        private ChatViewModel _selectedChat;
-
-        public ChatViewModel SelectedChat
-        {
-            get { return _selectedChat; }
-            set { _selectedChat = value; OnPropertyChanged(); }
-        }
+        private Chat _сhat;
 
         private IChatService<Chat> chatService = new ChatService<Chat>(new BreadgptDbContextFactory());
-        public ObservableCollection<Chat> Chats { get; } = new();
+        public ObservableCollection<Chat> Chats { get; }
+
+        private readonly NavigationService _navigationService;
+        public ICommand NavigateToChatCommand { get; }
 
         public ICommand CreateChatCommand { get; }
         public ICommand RenameChatCommand { get; }
         public ICommand DeleteChatCommand { get; }
         public ICommand SelectChatCommand { get; }
 
-        public MainViewModel()
+        public MainViewModel(NavigationService navigationService)
         {
-            LoadChats();
+            Chats = new ObservableCollection<Chat>();
+            _navigationService = navigationService;
 
             CreateChatCommand = new RelayCommand(CreateChat);
-            RenameChatCommand = new RelayCommand(DeleteChat);
+            RenameChatCommand = new RelayCommand(RenameChat);
             DeleteChatCommand = new RelayCommand(DeleteChat);
+
+            NavigateToChatCommand = new Commands.RelayCommand<ChatViewModel>(chat =>
+            {
+                if (chat != null)
+                {
+                    var chatPage = new ChatView
+                    {
+                        DataContext = chat
+                    };
+
+                    _navigationService.Navigate(chatPage);
+                }
+            });
+
+            LoadChats();
         }
 
         private void CreateChat()
         {
+            //if (_сhat.Messages.Count == 0) return;
             if (Chats.Count == 10) { MessageBox.Show("You have increased the maximum number of chats"); return; }
 
-            Chat chat;
             chatService.CreateAsync(
-                chat = new Chat
+                _сhat = new Chat
                 {
                     Id = Guid.NewGuid(),
                     Title = $"New Chat {Chats.Count + 1}",
                     LastMessageAt = DateTime.UtcNow
                 });
 
-            Chats.Add(chat);
+            Chats.Add(_сhat);
 
             SortChats();
         }
 
         private async void LoadChats()
         {
-            var chats = await chatService.GetAllAsync();
+            IEnumerable<Chat> chats = await chatService.GetAllAsync();
 
-            foreach(var chat in chats)
+            if (chats.Count() == 0) { CreateChat(); return; };
+
+            foreach (var chat in chats)
             {
                 Chats.Add(chat);
             }
@@ -74,6 +92,11 @@ namespace BreadGPT.ViewModels
             {
                 Chats.Add(chat);
             }
+        }
+
+        private void RenameChat()
+        {
+
         }
 
         private void DeleteChat()
