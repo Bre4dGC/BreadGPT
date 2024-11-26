5120d414e285aae6741a0d8128d9853cae3f5d42
Bre4dGC <glebbreadc@gmail.com>
2024-11-26 23:31:24 +0300

    Change name

---
+++ BreadGPT/Utilities/MessageBackgroundConverter.cs
@@ -0,0 +1,29 @@
+﻿using BreadGPT.Models;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BreadGPT.Styles
+{
+    class MessageBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is MessageSender.User)
+            {
+                return Brushes.LightBlue;
+            }
+            else if(value is MessageSender.ChatGPT)
+            {
+                return Brushes.LightGray;
+            }
+
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
