a52fbe8f2658f967dd362be4822b9afa93fd0c8e
Bre4dGC <glebbreadc@gmail.com>
2024-12-04 23:40:10 +0300

    Update Models

--- BreadGPT/Models/Chat.cs
+++ BreadGPT/Models/Chat.cs
@@ -3,8 +3,8 @@
     public class Chat : DomainObject
     {
         public string Title { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedAt { get; set; }
         public DateTime LastMessageAt { get; set; }
-        public virtual ICollection<Message> Messages { get; set; } = new List<Message>();
+        public ICollection<Message> Messages { get; set; } = new List<Message>();
     }
 }
--- BreadGPT/Models/Message.cs
+++ BreadGPT/Models/Message.cs
@@ -4,8 +4,7 @@
     {
         public Guid ChatId { get; set; }
         public string Text { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public MessageSender Sender { get; set; }
+        public bool IsSendByUser { get; set; }
         public virtual Chat Chat { get; set; }
     }
 }
