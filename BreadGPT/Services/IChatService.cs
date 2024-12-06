8a77d38111713640cbe4a449e47104f9389c6500
Bre4dGC <glebbreadc@gmail.com>
2024-12-06 23:52:41 +0300

    Removed Get method

--- BreadGPT/Services/IChatService.cs
+++ BreadGPT/Services/IChatService.cs
@@ -5,7 +5,6 @@ namespace BreadGPT.Services
     interface IChatService
     {
         Task<IEnumerable<Chat>> GetAll();
-        Task<Chat> Get(Guid id);
         Task<Chat> Create(Chat chat);
         Task<Chat> Update(Guid id, Chat chat);
         Task<bool> Delete(Guid id);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
